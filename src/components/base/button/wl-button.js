ff6c960f84ea6378180087824c82525e31e0b833
Craig Jennings <craig.jennings11@gmail.com>
2018-09-28 08:55:35 -0500

    Add 'wl-button' stories

--- src/components/base/button/wl-button.js
+++ src/components/base/button/wl-button.js
@@ -15,67 +15,61 @@ class WLButton extends LitElement {
     return html`
       <style>
         :host {
-          --button__bg-color: #eceff1;
-          --button__bg-color--hover: #cfd8dc;
-          --button__bg-color--active: #b0bec5;
-          --button-primary__bg-color: var(--primary-color);
-          --button-primary__bg-color--hover: var(--primary-color--light);
-          --button-primary__bg-color--active: var(--primary-color--dark);
-          --button-primary__color: #fafafa;
-
           display: contents;
         }
 
         button {
-          background-color: var(--button__bg-color);
+          background-color: var(--wl-button__bg-color, #eceff1);
           border-radius: calc(1.75rem / 2);
-          border: 1px solid #dddddd;
+          border-width: 0px;/* solid #dddddd; */
           height: 1.75rem;
-          line-height: 1.75rem;
+          /* line-height: 1.75rem; */
           padding: 0 1rem;
           transition: all .25s;
         }
 
         button:focus,
         button:hover {
-          background-color: var(--button__bg-color--hover);
+          background-color: var(--wl-button__bg-color--hover, #cfd8dc);
           cursor: pointer;
           outline: none;
         }
 
         button:active {
-          background-color: var(--button__bg-color--active);
+          background-color: var(--wl-button__bg-color--active, #b0bec5);
         }
 
         .primary {
-          background-color: var(--button-primary__bg-color);
-          border-color: var(--button-primary__bg-color);
-          color: var(--button-primary__color);
+          background-color: var(--wl-button--primary__bg-color, #1e88e5);
+          border-color: var(--wl-button--primary__bg-color, #1e88e5);
+          color: var(--wl-button--primary__color, #fafafa);
         }
 
         .primary:focus,
         .primary:hover {
-          background-color: var(--button-primary__bg-color--hover);
+          background-color: var(--wl-button--primary__bg-color--hover, #1976d2);
+          border-color: var(--wl-button--primary__bg-color--hover, #1976d2);
         }
 
         .primary:active {
-          background-color: var(--button-primary__bg-color--active);
+          background-color: var(--wl-button--primary__bg-color--active, #005cb2);
+          border-color: var(--wl-button--primary__bg-color--hover, #005cb2);
         }
 
-        /* .secondary {
-          background-color: var(--button-primary__bg-color);
-          border-color: var(--button-primary__bg-color);
-          color: var(--button-primary__color);
+        .secondary {
+          background-color: var(--wl-button--secondary__bg-color, #546e7a);
+          border-color: var(--wl-button--secondary__bg-color, #546e7a);
+          color: var(--wl-button--secondary__color, #fafafa);
         }
 
         .secondary:focus,
         .secondary:hover {
-          background-color: var(--button-primary__background-color--hover);
+          background-color: var(--wl-button--secondary__bg-color--hover, #819ca9);
         }
 
         .secondary:active {
-          background-color: var(--button-primary__background-color--active);
-        } */
+          background-color: var(--wl-button--secondary__bg-color--active, #29434e);
+        }
       </style>
     `;
   }
